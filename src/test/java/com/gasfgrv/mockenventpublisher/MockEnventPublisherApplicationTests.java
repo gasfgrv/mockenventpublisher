04e82e731d0436f26b9efc15dc29a7d4a00b25ef
Gustavo A. Silva <gustavoalmeidasilva41@gmail.com>
2025-11-09 17:49:42 -0300

    Teste de contexto

--- src/test/java/com/gasfgrv/mockenventpublisher/MockEnventPublisherApplicationTests.java
+++ src/test/java/com/gasfgrv/mockenventpublisher/MockEnventPublisherApplicationTests.java
@@ -2,14 +2,28 @@ package com.gasfgrv.mockenventpublisher;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.ApplicationContext;
 
-@Import(TestcontainersConfiguration.class)
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class MockEnventPublisherApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    private final ApplicationContext applicationContext;
+
+    MockEnventPublisherApplicationTests(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @Test
+    void contextLoads() {
+        Set<String> beans = Arrays.stream(applicationContext.getBeanDefinitionNames()).collect(Collectors.toSet());
+        assertTrue(beans.contains("kafkaTemplate"));
+        assertTrue(beans.contains("schemaRegistryClient"));
+    }
 
 }
